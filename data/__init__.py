a33f90d8d5f4253c271114dff38c71f3d626c987
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2021-02-10 17:21:46 +0100

    blegh

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,5 @@
 from .data import simple_df, extended_df
 from .dataloader import make_dataloader
 
-df = simple_df
-# df = extended_df
-
-dataloader = make_dataloader(df)
+simple_dataloader = make_dataloader(simple_df)
+extended_dataloader = make_dataloader(extended_df)
--- models/hopfield.py
+++ models/hopfield.py
@@ -1,9 +1,10 @@
 import torch
 
 
-class Hopfield(torch.nn.Module):
-    def __init__(self, hidden_dim: int, recurrent_generations: int):
+class HopfieldLayer(torch.nn.Module):
+    def __init__(self, hidden_dim: int, recurrent_generations: int, F=torch.relu):
         super().__init__()
+        self.F = F
         self.recurrent_generations = recurrent_generations
         self.linear = torch.nn.Linear(hidden_dim, hidden_dim)
         torch.nn.init.uniform_(self.linear.weight, -1, 1)
@@ -26,3 +27,23 @@ class Hopfield(torch.nn.Module):
     @property
     def bias(self):
         return self.linear.bias
+
+
+class Hopfield(torch.nn.Module):
+    def __init__(self, in_dim: int, hidden_dim: int, out_dim: int, recurrent_generations: int, F=torch.sigmoid) -> None:
+        super().__init__()
+        self.F = F
+        self.linear1 = torch.nn.Linear(in_dim, hidden_dim)
+        self.hopfield = HopfieldLayer(hidden_dim, recurrent_generations)
+        self.linear2 = torch.nn.Linear(hidden_dim, out_dim)
+
+    def forward(self, x: torch.Tensor):
+        h = self.F(self.linear1(x))
+        rec = self.hopfield(h)
+        y_pred = self.F(self.linear2(rec))
+        return y_pred
+
+    def reset_parameters(self):
+        self.linear1.reset_parameters()
+        self.hopfield.reset_parameters()
+        self.linear2.reset_parameters()
